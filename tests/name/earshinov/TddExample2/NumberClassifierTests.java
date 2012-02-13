afc5229cf5e7b20a2f62203b2341d76c7ccd4026
Eugene Arshinov <earshinov@gmail.com>
2012-02-13 21:46:47 +0400

    Тесты, что -1 и -6 - не совершенные числа.

--- tests/name/earshinov/TddExample2/NumberClassifierTests.java
+++ tests/name/earshinov/TddExample2/NumberClassifierTests.java
@@ -7,29 +7,41 @@ import org.junit.Test;
 public class NumberClassifierTests {
 
 	@Test
+	public void test_minus_6_is_not_perfect() {
+		NumberClassifier classifier = new NumberClassifier(-6);
+		assertFalse(classifier.numberIsPerfect());
+	}
+
+	@Test
+	public void test_minus_1_is_not_perfect() {
+		NumberClassifier classifier = new NumberClassifier(-1);
+		assertFalse(classifier.numberIsPerfect());
+	}
+
+	@Test
 	public void test_0_is_not_perfect() {
 		NumberClassifier classifier = new NumberClassifier(0);
 		assertFalse(classifier.numberIsPerfect());
 	}
-	
+
 	@Test
 	public void test_1_is_perfect() {
 		NumberClassifier classifier = new NumberClassifier(1);
 		assertTrue(classifier.numberIsPerfect());
 	}
-	
+
 	@Test
 	public void test_6_is_perfect() {
 		NumberClassifier classifier = new NumberClassifier(6);
 		assertTrue(classifier.numberIsPerfect());
 	}
-	
+
 	@Test
 	public void test_12_is_not_perfect() {
 		NumberClassifier classifier = new NumberClassifier(12);
 		assertFalse(classifier.numberIsPerfect());
 	}
-	
+
 	@Test
 	public void test_28_is_perfect() throws Exception {
 		NumberClassifier classifier = new NumberClassifier(28);
